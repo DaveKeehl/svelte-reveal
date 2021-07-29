581eb79e752fde934cdcc538160d8730de9ba625
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-30 00:36:18 +0200

    Not minifying files for now

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,7 +4,7 @@ build({
 	entryPoints: ['src/index.ts'],
 	outfile: 'dist/index.js',
 	bundle: true,
-	minify: true,
+	minify: false,
 	sourcemap: true,
 	format: 'esm',
 	watch: process.argv[2] === '--watch'
