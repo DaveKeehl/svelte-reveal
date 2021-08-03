a5e549bfb33ef16df74748f7ecf0b8c9a4487e42
Davide Ciulla <davide.ciulla@hotmail.com>
2021-08-03 12:15:41 +0200

    Fixed test

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,8 +1,8 @@
-import type { CustomEase } from '../src/types';
+import type { CustomEasing } from '../src/types';
 import { getEasing } from '../src/utils';
 
-const customEase: CustomEase = [0.2, 0.8, 1, 0.2];
+const customEasing: CustomEasing = [0.2, 0.8, 1, 0.2];
 
 test('Cubic-bezier easing function requires array of values', () => {
-	expect(getEasing('linear', customEase)).toBe('linear');
+	expect(getEasing('linear', customEasing)).toBe('linear');
 });
