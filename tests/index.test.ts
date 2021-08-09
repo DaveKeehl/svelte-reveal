2744a73da05f5218ebc3171ee3526c8c950ab840
Davide Ciulla <davide.ciulla@hotmail.com>
2021-08-09 23:45:08 +0200

    Testing init options

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,5 @@
 import {
+	init,
 	config,
 	setDev,
 	setOnce,
@@ -6,8 +7,7 @@ import {
 	setObserverRoot,
 	setObserverRootMargin,
 	setObserverThreshold,
-	setConfig,
-	reveal
+	setConfig
 } from '../src/index';
 import type { IConfig, IObserverOptions } from '../src/types';
 
@@ -138,8 +138,31 @@ describe('Testing API correctness', () => {
 	});
 });
 
-// describe('Reveal', () => {
-// 	test('Options have correct values', () => {
+test('Test initial option values', () => {
+	expect(init.disable).toBe(false);
+	expect(init.debug).toBe(false);
+	expect(init.ref).toBe('');
+	expect(init.highlightLogs).toBe(false);
+	expect(init.highlightColor).toBe('tomato');
+	expect(init.root).toBe(null);
+	expect(init.marginTop).toBe(0);
+	expect(init.marginBottom).toBe(0);
+	expect(init.marginLeft).toBe(0);
+	expect(init.marginRight).toBe(0);
+	expect(init.threshold).toBe(0.6);
+	expect(init.transition).toBe('fly');
+	expect(init.delay).toBe(0);
+	expect(init.duration).toBe(800);
+	expect(init.easing).toBe('custom');
+	expect(init.customEasing).toStrictEqual([0.25, 0.1, 0.25, 0.1]);
+	expect(init.x).toBe(-20);
+	expect(init.y).toBe(-20);
+	expect(init.deg).toBe(-360);
 
-// 	})
-// })
+	const node = document.createElement('p');
+	expect(init.onRevealStart(node)).toBe(null);
+	expect(init.onRevealEnd(node)).toBe(null);
+	expect(init.onMount(node)).toBe(null);
+	expect(init.onUpdate(node)).toBe(null);
+	expect(init.onDestroy(node)).toBe(null);
+});
