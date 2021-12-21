deecccdaeca69a46186a89aaf8640aec9f2e5c33
Davide Ciulla <davide.ciulla@hotmail.com>
2021-12-21 21:14:07 +0100

    Added new DOM tests

---
+++ tests/DOM.test.ts
@@ -0,0 +1,46 @@
+import { createObserver } from '../src/internal/DOM';
+import { init } from '../src/internal/config';
+
+export function setupIntersectionObserverMock({
+	root = null,
+	rootMargin = '',
+	thresholds = [],
+	disconnect = () => null,
+	observe = () => null,
+	takeRecords = () => [],
+	unobserve = () => null
+} = {}): void {
+	class MockIntersectionObserver implements IntersectionObserver {
+		readonly root: Element | null = root;
+		readonly rootMargin: string = rootMargin;
+		readonly thresholds: ReadonlyArray<number> = thresholds;
+		disconnect: () => void = disconnect;
+		observe: (target: Element) => void = observe;
+		takeRecords: () => IntersectionObserverEntry[] = takeRecords;
+		unobserve: (target: Element) => void = unobserve;
+	}
+
+	Object.defineProperty(window, 'IntersectionObserver', {
+		writable: true,
+		configurable: true,
+		value: MockIntersectionObserver
+	});
+
+	Object.defineProperty(global, 'IntersectionObserver', {
+		writable: true,
+		configurable: true,
+		value: MockIntersectionObserver
+	});
+}
+
+beforeEach(() => {
+	setupIntersectionObserverMock();
+});
+
+describe('createObserver', () => {
+	test('The function returns an IntersectionObserver object', () => {
+		const node = document.createElement('div');
+		const res = createObserver(true, '#000000', node, init, 'css_class');
+		expect(res).toBeInstanceOf(IntersectionObserver);
+	});
+});
