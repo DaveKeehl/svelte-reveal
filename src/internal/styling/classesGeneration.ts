b59c81eefe8eb432317608f32bff5d7ab94b6695
Davide Ciulla <davide.ciulla@hotmail.com>
2022-04-22 21:47:56 +0200

    Removed vendors

--- src/internal/styling/classesGeneration.ts
+++ src/internal/styling/classesGeneration.ts
@@ -44,14 +44,13 @@ export const createMainCss = (className: string, options: Required<IOptions>): s
 export const createTransitionCss = (className: string, options: Required<IOptions>) => {
 	const { duration, delay, easing, customEasing } = options;
 
-	let styles = `
+	const styles = `
 		transition: all ${duration / 1000}s ${delay / 1000}s ${getEasing(easing, customEasing)};
 	`;
-	styles = addVendors(styles.trim());
 
 	return `
 		.${className} {
-			${styles}
+			${styles.trim()}
 		}
 	`;
 };
