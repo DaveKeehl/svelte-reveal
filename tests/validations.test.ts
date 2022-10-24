60ad3b4ade12f2bfd88065e151926fea750fd432
Davide Ciulla <davide.ciulla@hotmail.com>
2022-10-24 20:28:34 +0200

    Added test to catch erroneous options being used

--- tests/validations.test.ts
+++ tests/validations.test.ts
@@ -1,4 +1,5 @@
-import { hasValidRange, isPositive, isPositiveInteger } from '../src/internal/validations';
+import type { IOptions } from '../src/internal/types';
+import { checkOptions, hasValidRange, isPositive, isPositiveInteger } from '../src/internal/validations';
 
 test('hasValidRange', () => {
 	expect(hasValidRange(100, 0, 200)).toBe(true);
@@ -18,4 +19,40 @@ test('isPositiveInteger', () => {
 	expect(isPositiveInteger(-1)).toBe(false);
 	expect(isPositiveInteger(5.5)).toBe(false);
 	expect(isPositiveInteger(-5.5)).toBe(false);
+});
+
+test('checkOptions', () => {
+	const userOptions: IOptions = {
+		transition: 'fly',
+		debug: true,
+		ref: 'a',
+		threshold: 0.2,
+		marginBottom: 100,
+		marginTop: 100
+	};
+	const options = checkOptions(userOptions);
+
+	expect(options.disable).toBe(false);
+	expect(options.debug).toBe(true);
+	expect(options.ref).toBe('a');
+	expect(options.highlightLogs).toBe(false);
+	expect(options.highlightColor).toBe('tomato');
+	expect(options.root).toBe(null);
+	expect(options.marginTop).toBe(100);
+	expect(options.marginBottom).toBe(100);
+	expect(options.marginLeft).toBe(0);
+	expect(options.marginRight).toBe(0);
+	expect(options.threshold).toBe(0.2);
+	expect(options.transition).toBe('fly');
+	expect(options.reset).toBe(false);
+	expect(options.delay).toBe(0);
+	expect(options.duration).toBe(800);
+	expect(options.easing).toBe('custom');
+	expect(options.customEasing).toStrictEqual([0.25, 0.1, 0.25, 0.1]);
+	expect(options.x).toBe(-20);
+	expect(options.y).toBe(-20);
+	expect(options.rotate).toBe(-360);
+	expect(options.opacity).toBe(0);
+	expect(options.blur).toBe(16);
+	expect(options.scale).toBe(0);
 });
