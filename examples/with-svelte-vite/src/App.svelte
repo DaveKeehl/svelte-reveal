1155ddebd0e22d33c986d61aa18cc8a269fc149e
Davide Ciulla <davide.ciulla@hotmail.com>
2024-03-19 18:37:11 +0100

    Ran formatter

--- examples/with-svelte-vite/src/App.svelte
+++ examples/with-svelte-vite/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from './assets/svelte.svg';
+  import viteLogo from '/vite.svg';
+  import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
@@ -20,12 +20,11 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the
+    official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
--- examples/with-svelte-vite/src/lib/Counter.svelte
+++ examples/with-svelte-vite/src/lib/Counter.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
 <button on:click={increment}>
