dfd6133b5cb2880e5f13b6b07ff4dd23bdd14887
Davide Ciulla <davide.ciulla@hotmail.com>
2024-03-25 22:31:30 +0100

    Updated example

--- examples/with-svelte-vite/src/App.svelte
+++ examples/with-svelte-vite/src/App.svelte
@@ -1,47 +1,12 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from '/vite.svg';
-  import Counter from './lib/Counter.svelte';
   import { reveal } from 'svelte-reveal';
 </script>
 
 <main>
-  <div use:reveal>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the
-    official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <h1 use:reveal>Fade Transition</h1>
+  <h1 use:reveal={{ transition: 'fly' }}>Fly Transition</h1>
+  <h1 use:reveal={{ transition: 'slide' }}>Slide Transition</h1>
+  <h1 use:reveal={{ transition: 'blur' }}>Blur Transition</h1>
+  <h1 use:reveal={{ transition: 'spin' }}>Spin Transition</h1>
+  <h1 use:reveal={{ transition: 'scale', scale: 3 }}>Scale Transition</h1>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
