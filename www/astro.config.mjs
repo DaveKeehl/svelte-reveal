de2aa00c889b8d73cf6956be5ab76f8358e06082
Davide Ciulla <davide.ciulla@hotmail.com>
2024-04-11 00:13:44 +0200

    Started working on new website documentation

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -5,23 +5,10 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'Svelte Reveal',
       social: {
-        github: 'https://github.com/withastro/starlight'
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' }
-          ]
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' }
-        }
-      ]
+        github: 'https://github.com/davekeehl/svelte-reveal'
+      }
     })
   ]
 });
