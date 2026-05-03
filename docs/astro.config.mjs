ce49d4b8bb256c9ae9552ec233f3f2a159a9eb8e
Davide Wölckner <davide.woelckner@gmail.com>
2026-05-03 14:46:27 +0200

    chore(docs): set astro site for sitemap generation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://svelte-reveal.vercel.app',
   integrations: [
     starlight({
       title: 'Svelte Reveal',
